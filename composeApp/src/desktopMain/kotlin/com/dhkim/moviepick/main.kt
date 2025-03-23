b0a83465d51f9953f923b3e8a00a3e2c7c718820
dohyun <dnlwkem39@gmail.com>
2025-03-23 12:36:25 +0900

    iOS, Desktop 빌드 오류 대응

--- composeApp/src/desktopMain/kotlin/com/dhkim/moviepick/main.kt
+++ composeApp/src/desktopMain/kotlin/com/dhkim/moviepick/main.kt
@@ -1,11 +1,15 @@
 package com.dhkim.moviepick
 
+import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.dhkim.core.designsystem.MoviePickTheme
 import com.dhkim.di.initKoin
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@ExperimentalCoroutinesApi
+@ExperimentalSharedTransitionApi
 fun main() {
     initKoin()
     application {
--- composeApp/src/iosMain/kotlin/com/dhkim/moviepick/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/dhkim/moviepick/MainViewController.kt
@@ -1,11 +1,15 @@
 package com.dhkim.moviepick
 
+import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.ui.window.ComposeUIViewController
 import com.dhkim.core.designsystem.MoviePickTheme
 import com.dhkim.di.initKoin
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import platform.UIKit.UIScreen
 import platform.UIKit.UIUserInterfaceStyle
 
+@ExperimentalCoroutinesApi
+@ExperimentalSharedTransitionApi
 fun MainViewController() = ComposeUIViewController(
     configure = {
         initKoin()
